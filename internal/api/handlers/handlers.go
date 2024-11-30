e252bd267c47bdcc0220e78e1b69836f26ee95a6
Anh Cao <anhcao4922@gmail.com>
2024-11-30 21:24:42 +0200

    feat: implement Handler struct

--- internal/api/handlers/handlers.go
+++ internal/api/handlers/handlers.go
@@ -3,22 +3,48 @@
 package handlers
 
 import (
+	"fmt"
 	"net/http"
 
-	"github.com/AnhCaooo/electric-notifications/internal/logger"
+	"github.com/AnhCaooo/electric-notifications/internal/cache"
+	"github.com/AnhCaooo/electric-notifications/internal/db"
+	"github.com/AnhCaooo/electric-notifications/internal/firebase"
 	"go.uber.org/zap"
 )
 
+type Handler struct {
+	logger   *zap.Logger
+	cache    *cache.Cache
+	mongo    *db.Mongo
+	firebase *firebase.Firebase
+}
+
+// NewHandler returns a new Handler instance
+func NewHandler(logger *zap.Logger, cache *cache.Cache, mongo *db.Mongo, firebase *firebase.Firebase) *Handler {
+	// todo: validate ?
+	return &Handler{
+		logger:   logger,
+		cache:    cache,
+		mongo:    mongo,
+		firebase: firebase,
+	}
+}
+
 // return response when request url is not found
-func NotFound(w http.ResponseWriter, r *http.Request) {
+func (h Handler) NotFound(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusNotFound)
-	logger.Logger.Info("undefined endpoint", zap.String("method", r.Method), zap.String("endpoint", r.URL.Path))
+	h.logger.Info("undefined endpoint", zap.String("method", r.Method), zap.String("endpoint", r.URL.Path))
 	w.Write([]byte("404 - Not found"))
 }
 
 // return response when request method is not allowed
-func NotAllowed(w http.ResponseWriter, r *http.Request) {
+func (h Handler) NotAllowed(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusMethodNotAllowed)
-	logger.Logger.Info("method not allowed", zap.String("method", r.Method), zap.String("endpoint", r.URL.Path))
+	h.logger.Info("method not allowed", zap.String("method", r.Method), zap.String("endpoint", r.URL.Path))
 	w.Write([]byte("405 - Method not allowed"))
+}
+
+// Ping the connection to the server
+func (h Handler) Ping(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "pong")
 }
